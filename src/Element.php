9b0a1ea048aca9cc245aedc0a5601c6d29b4cc8a
Loduis Madariaga <loduis@gmail.com>
2021-05-10 17:30:34 -0500

    add fromArray.

--- src/Element.php
+++ src/Element.php
@@ -4,6 +4,11 @@ namespace XML\Support;
 
 class Element extends DataAccess
 {
+    public static function fromArray(array $data): static
+    {
+        return new static($data);
+    }
+
     public function toArray()
     {
         return $this->data;
